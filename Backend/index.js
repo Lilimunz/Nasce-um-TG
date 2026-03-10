7b7afb446fc22d23d4057cc31fa59c4e9c88a478
Alicia Muniz <alicia.muniz@gestaopublica.etc.br>
2026-03-10 13:16:25 -0300

    feat: conexão com banco de dados, criptografia senha, insert e select (pessoa ja cadastrada)

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,61 @@
-import express from 'express'
+require('dotenv').config()
+
+const bcrypt = require('bcrypt')
+const cors = require ('cors')
+const express = require('express')
+const mysql = require('mysql2')
 const app = express()
+app.use(express.json())
+app.use(cors())
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err.stack);
+        return;
+    }
+    console.log('Conexão bem-sucedida ao banco de dados com o ID: ', connection.threadId);
+});
+
+app.post('/tutor', async (req, res) => {
+    try {
+        const nome = req.body.nome
+        const email = req.body.email
+        const senha = req.body.senha
+        const hasheada = await bcrypt.hash(senha, 10)
+        const insert = 'INSERT INTO tb_tutor (nome, email, senha) VALUES (?, ?, ?)'
+        connection.query(insert, [nome, email, hasheada], (err, results, fields) => {
+            console.log(results)
+            console.log(fields)
+            res.send('Da um select no banco pra ter certeza')
+        })
+    } catch (error) {
+        console.log('Algo deu errado')
+    }
+})
+
+app.post('/login', (req, res) => {
+    const email = req.body.email
+    const senha = req.body.senha
+    const user = 'SELECT * FROM tb_tutor WHERE email = ?'
+    connection.query(user, [email], async (err, results, fields) => {
+        if (results.length === 0) {
+            return res.status(404).json({ error: "Usuário não encontrado" })
+        }
+        const compuse = results[0]
+        const senhaIgual = await bcrypt.compare(senha, compuse.senha)
+        if (!senhaIgual) {
+            return res.status(401).json({ mensagem: "senha inválida" })
+        }
+        res.end()
+    })
+})
 
 app.get('/hello', (req, res) => {
     res.send('Olá Mundo')
